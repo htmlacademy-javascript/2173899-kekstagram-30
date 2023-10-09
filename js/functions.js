c3b9e7a65769a239da254b0e87cd54fb590779b0
Сергей Сакович <imperator42@rambler.ru>
2023-10-09 22:03:45 +0300

    Вносит правки после review

--- js/functions.js
+++ js/functions.js
@@ -1,31 +1,27 @@
 // Функция для проверки длины строки
 
-function checksStringLength(string, number) {
-  return (string.length <= number);
-}
+const checkStringLength = (string, number) => (string.length <= number);
 
-checksStringLength('I like JavaScript', 22);
+checkStringLength('I like JavaScript', 22);
 
 
 // Функция для проверки, является ли строка палиндромом 1й способ.
 
 
-function palindromeChecker(string) {
+const isPalindrome = (string) => {
   const stringChecked = string.replaceAll(/\s/g, '').toLowerCase();
   let stringPalindrom = '';
   for (let i = stringChecked.length - 1; i >= 0; i--) {
     stringPalindrom += stringChecked[i];
   }
   return stringChecked === stringPalindrom;
-}
+};
 
-palindromeChecker('Город дорог');
+isPalindrome('Город дорог');
 
 
 // 2й способ.
-function palindrome(string) {
-  return string === string.split('').reverse().join('');
-}
+const palindrome = (string) => string === string.split('').reverse().join('');
 
 palindrome('racecar');
 
@@ -34,14 +30,14 @@ palindrome('racecar');
 
 /* Функция принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа. Если в строке нет ни одной цифры, функция возврвщает NaN
 */
-function returnNumber(string) {
+const returnNumber = (string) => {
   const numb = string.match(/\d/g);
   if (numb) {
     return parseInt(numb.join(''), 10);
   } else {
     return NaN;
   }
-}
+};
 
 returnNumber('ECMAScript 2022');
 
