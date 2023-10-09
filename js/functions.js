2e33bdcb38b5c6d8d71733a58dd8a088d2832dc8
Сергей Сакович <imperator42@rambler.ru>
2023-10-09 05:38:05 +0300

    Вносит правки

--- js/functions.js
+++ js/functions.js
@@ -11,12 +11,12 @@ checksStringLength('I like JavaScript', 22);
 
 
 function palindromeChecker(string) {
-  const isStringPalindrom = string.replaceAll(/\s/g, '').toLowerCase();
-  let isPalindromNew = '';
-  for (let i = isStringPalindrom.length - 1; i >= 0; i--) {
-    isPalindromNew += isStringPalindrom[i];
+  const stringChecked = string.replaceAll(/\s/g, '').toLowerCase();
+  let stringPalindrom = '';
+  for (let i = stringChecked.length - 1; i >= 0; i--) {
+    stringPalindrom += stringChecked[i];
   }
-  return isStringPalindrom === isPalindromNew;
+  return stringChecked === stringPalindrom;
 }
 
 palindromeChecker('Город дорог');
@@ -30,6 +30,18 @@ function palindrome(string) {
 palindrome('racecar');
 
 
-// Дополнительное задание не смог сделать, наверное нужно отдохнуть!
+// Дополнительное задание
 
+/* Функция принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа. Если в строке нет ни одной цифры, функция возврвщает NaN
+*/
+function returnNumber(string) {
+  const numb = string.match(/\d/g);
+  if (numb) {
+    return parseInt(numb.join(''), 10);
+  } else {
+    return NaN;
+  }
+}
+
+returnNumber('ECMAScript 2022');
 
