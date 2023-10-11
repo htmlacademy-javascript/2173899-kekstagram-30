8654e9570b566e307d1941ae7592b9ac8cea5359
Сергей Сакович <imperator42@rambler.ru>
2023-10-11 21:12:36 +0300

    Вносит правки

--- js/functions.js
+++ js/functions.js
@@ -20,23 +20,18 @@ const isPalindrome = (string) => {
 isPalindrome('Город дорог');
 
 
-// 2й способ.
-const palindrome = (string) => string === string.split('').reverse().join('');
-
-palindrome('racecar');
-
-
 // Дополнительное задание
 
 /* Функция принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа. Если в строке нет ни одной цифры, функция возврвщает NaN
 */
 const returnNumber = (string) => {
-  const numb = string.match(/\d/g);
+  const numb = string.toString().match(/\d/g);
   if (numb) {
     return parseInt(numb.join(''), 10);
   } else {
     return NaN;
   }
+
 };
 
 returnNumber('ECMAScript 2022');
