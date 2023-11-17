a3028dcaa28f8cd8c9e2ad25b01546ab9b2b9d3b
Сергей Сакович <imperator42@rambler.ru>
2023-11-17 17:50:22 +0300

    Добавляет возможность отфильтровывать фотографии

--- js/main.js
+++ js/main.js
@@ -3,11 +3,13 @@ import {renderThumbnails} from './thumbnails.js';
 import { onFileInputChange } from './upload-new-picture.js';
 import { loadData } from './api.js';
 import { showErrorMessage } from './utils.js';
+import { initFilter } from './filters.js';
 
 const bootstrap = async () => {
   try {
     const pictures = await loadData();
     renderThumbnails(pictures);
+    initFilter(pictures);
   } catch (error) {
     showErrorMessage();
   }
